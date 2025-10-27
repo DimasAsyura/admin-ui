a5ba04396133d1fd163003704d160bdf188e64cd
DimasAsyura <111202314937@mhs.dinus.ac.id>
2025-10-28 00:42:37 +0700

    update tugas 6

---
+++ src/Exercise.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import UserCard from "./UserCard";
+
+function Exercise() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+          User Cards
+        </h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+          <UserCard 
+          name="dimzy" 
+          email="dimzy@gmail.com"
+          street="Jl. Lempongsari"
+          city="Semarang"
+        />
+          <UserCard 
+          name="asyura" 
+          email="asyura@gmail.com"
+          street="Jl. Veteran"
+          city="Semarang"
+        />
+          <UserCard 
+          name="mus"
+          email="mus@gmail.com"
+          street="Jl. Raya"
+          city="Bandung" 
+        />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Exercise;
